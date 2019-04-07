ffe126a5015b57dce9d924c46809141173127ceb
esarder <45513953+esarder@users.noreply.github.com>
2019-04-07 12:28:00 -0600

    Add files via upload

---
+++ UserHashTable.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "ariadne(modElla).hpp"
+
+using namespace std;
+
+class HashTable {
+public:
+  HashTable(int tableSize);
+  ~HashTable();
+
+  // Overload add user to start with multiple kinds of data? Do we only want to call it after we have all the user's information, or
+  // let them add it later on?
+  void addUser(string _username);
+
+  void deleteUser(string _username);
+
+  // Check to make sure usernmame isn't already taken
+  bool isInTable(string _username);
+
+  int returnCollisions();
+
+  // Returns total number of users in hashtable
+  int returnTotalUsers();
+
+private:
+  int hash_func(string username);
+  User* searchTable(string _username);
+
+  User** userHashTable;
+
+  int tableSize;
+  int numUsers;
+  int numCollisions;
+
+
+};
+
+const int USER_TABLE_SIZE = 100;
---
+++ ariadne(modElla).hpp
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef CLASS_HPP
+#define CLASS_HPP
+
+
+#include <iostream>
+
+using namespace std;
+
+struct Event
+{
+  string event;
+  int timer;
+  int index;
+};
+
+class User
+{
+  private:
+    string username;
+    string userFirstName;
+    string userLastName;
+    int pin;
+    string EC_firstName;
+    string EC_lastName;
+    string EC_email;
+    Event* eventsArray[3];
+
+  public:
+    User();
+    ~User();
+
+    string getUsername();
+    string getUserFirstName();
+    string getUserLastName();
+    int getPin();
+    string getEC_firstName();
+    string getEC_lastName();
+    string getEC_email();
+
+    void setUsername(string username);
+    void setUser(string firstName, string lastName);
+    void setPin(int pin);
+    void setEC_name(string firstName, string lastName);
+    void setEC_email(string email);
+
+    bool deactivateAlert(string username, int pin);
+    void printEvents(string username);
+    void addEvent(string username, string event, int pin);
+
+    User* next;
+};
+
+#endif
